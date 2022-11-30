71bfce324b8e807d18d391dd057adbdf164adea7
Andrew McFadden <mcfadden.andrewj@gmail.com>
2022-11-30 16:42:43 -0700

    Adding JS script for game functionality and HTML game rules

---
+++ script.js
@@ -0,0 +1,19 @@
+// The following script defines  a game of Rock, Paper, Scissors in JavaScript. It is written for a project in The Odin Project foundations course. 
+
+// The script is called and run from the rock-paper-scissors.html file, with output displayed in the browser console. 
+
+// Define Variables
+const min = 1;
+const max = 3;
+
+let playerChoice;
+let computerChoice; 
+let compareChoices;
+let endRound;
+
+// Declare functions
+
+
+
+// Create game logic 
+
